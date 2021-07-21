13e9ded1ce476a8d9e2331234649a167db8fa145
898mm <87744934+898mm@users.noreply.github.com>
2021-07-21 12:45:54 +0800

    Add files via upload

---
+++ iReSignTimeLock/iReSignTimeLock/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iReSignTimeLock
+//
+//  Copyright © 2019 appulize. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ iReSignTimeLock/iReSignTimeLock/main.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  iReSignTimeLock
+//
+//  Copyright © 2019 appulize. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
